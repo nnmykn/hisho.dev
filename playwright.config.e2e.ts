0edacecf496b1c5d62b7750cad95f2d16991aa0d
hisho <hisho.web@gmail.com>
2022-12-26 19:48:22 +0900

    🔧 testファイルが追加pathを追加

---
+++ playwright.config.e2e.ts
@@ -0,0 +1,31 @@
+import { type PlaywrightTestConfig, devices } from '@playwright/test'
+import dotenv from 'dotenv'
+import path from 'path'
+
+dotenv.config()
+
+dotenv.config({ path: path.resolve(__dirname, '.env.local') })
+
+const NEXT_PUBLIC_FRONTEND_URL =
+  process.env['NEXT_PUBLIC_FRONTEND_URL'] ?? 'http://localhost:3000'
+
+const config: PlaywrightTestConfig = {
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+  ],
+  testMatch: '{src,app}/**/*.e2e.spec.ts',
+  use: {
+    baseURL: NEXT_PUBLIC_FRONTEND_URL,
+    headless: true,
+  },
+  webServer: {
+    command: 'yarn dev',
+    reuseExistingServer: true,
+    url: NEXT_PUBLIC_FRONTEND_URL,
+  },
+}
+
+export default config
