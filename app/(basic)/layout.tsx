5b3b929940d08ac112e433b28954c61628d74838
hisho <hisho.web@gmail.com>
2023-01-29 22:09:06 +0900

    💄 basic layoutにHeroを読み込ませた

--- app/(basic)/layout.tsx
+++ app/(basic)/layout.tsx
@@ -1,6 +1,7 @@
 import type { ReactNode } from 'react'
 import { Header } from '@src/component/Header/Header'
 import { Footer } from '@src/component/Footer/Footer'
+import { Hero } from '@app/(basic)/Hero'
 
 type Props = {
   children: ReactNode
@@ -8,8 +9,13 @@ type Props = {
 
 const Layout = ({ children }: Props) => {
   return (
-    <div className={'grid grid-rows-[auto_1fr_auto] min-h-[100svh] @container'}>
+    <div
+      className={
+        'grid grid-rows-[auto_auto_1fr_auto] min-h-[100svh] @container'
+      }
+    >
       <Header />
+      <Hero />
       <main className={'wrapper pt-4 pb-10 @[600px]:pb-20'}>{children}</main>
       <Footer />
     </div>
