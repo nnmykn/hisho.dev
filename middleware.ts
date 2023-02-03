dd17f24ab9574be449c372a59e50f4c62719dcaa
hisho <hisho.web@gmail.com>
2023-02-03 15:19:15 +0900

    ✨ Basic Authを追加

---
+++ middleware.ts
@@ -0,0 +1,28 @@
+import { NextRequest, NextResponse } from 'next/server'
+
+export const config = {
+  matcher: ['/', '/index'],
+}
+
+/**
+ * @see https://github.com/vercel/examples/blob/main/edge-middleware/basic-auth-password/middleware.ts
+ */
+export function middleware(req: NextRequest) {
+  const basicAuth = req.headers.get('authorization')
+  const url = req.nextUrl
+
+  if (basicAuth) {
+    const authValue = basicAuth.split(' ')[1]
+    const [user, pwd] = atob(authValue ?? '').split(':')
+
+    if (
+      user === process.env['BASIC_AUTH_USERNAME'] &&
+      pwd === process.env['BASIC_AUTH_PASSWORD']
+    ) {
+      return NextResponse.next()
+    }
+  }
+  url.pathname = '/api/auth'
+
+  return NextResponse.rewrite(url)
+}
