537151f86dd46c39ba753f2f2583090092ffecad
hisho <hisho.web@gmail.com>
2023-02-03 22:00:20 +0900

    💥 middlewareからrewriteに変更した

--- next.config.js
+++ next.config.js
@@ -1,10 +1,25 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  experimental: {
-    appDir: true,
-  },
-  pageExtensions: ['api.ts', 'tsx'],
+const nextConfig = () => {
+  const __PROD__ = process.env.NODE_ENV === 'production'
+
+  return {
+    reactStrictMode: true,
+    experimental: {
+      appDir: true,
+    },
+    pageExtensions: ['api.ts', 'tsx'],
+    async redirects() {
+      return __PROD__
+        ? [
+            {
+              source: '/:path((?!404$).*)',
+              destination: '/404',
+              permanent: true,
+            },
+          ]
+        : []
+    },
+  }
 }
 
-module.exports = nextConfig
+module.exports = nextConfig()
