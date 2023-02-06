43d1fc29e22eb1fb3027c9b3ab15486b56756539
hisho <hisho.web@gmail.com>
2023-02-06 10:47:30 +0900

    💥 migrate jest -> vitest

--- src/component/Header/Header.component.spec.tsx
+++ src/component/Header/Header.component.spec.tsx
@@ -1,5 +1,6 @@
 import { Header } from '@src/component/Header/Header'
 import { render, screen } from '@testing-library/react'
+import { describe, test, expect } from 'vitest'
 
 describe('Headerコンポーネントのテスト', () => {
   render(<Header />)
---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import react from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths'
+import { defineConfig } from 'vite'
+import { configDefaults } from 'vitest/config'
+import path from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), tsconfigPaths()],
+  test: {
+    globals: true,
+    environment: 'happy-dom',
+    setupFiles: path.resolve(process.cwd(), './vitest.setup.ts'),
+    exclude: [
+      ...configDefaults.exclude,
+      path.resolve(process.cwd(), './src/app/**/page.e2e.spec.ts'),
+    ],
+  },
+})
