f59cddf930083f645b91ea306509405710ff3c8a
hisho <hisho.web@gmail.com>
2023-02-06 19:20:44 +0900

    ✨ ctxにreqのheaderを含めるように

--- backend/context.ts
+++ backend/context.ts
@@ -1,28 +1,18 @@
-import type * as trpc from '@trpc/server'
-import type * as trpcNext from '@trpc/server/adapters/next'
-
-interface CreateContextOptions {
-  // session: Session | null
-}
-
-/**
- * Inner function for `createContext` where we create the context.
- * This is useful for testing when we don't want to mock Next.js' request/response
- */
-export async function createContextInner(_opts: CreateContextOptions) {
-  return {}
-}
-
-export type Context = trpc.inferAsyncReturnType<typeof createContextInner>
+import type { inferAsyncReturnType } from '@trpc/server'
+import type { CreateNextContextOptions } from '@trpc/server/adapters/next'
 
 /**
  * Creates context for an incoming request
  * @link https://trpc.io/docs/context
  */
-export async function createContext(
-  _opts: trpcNext.CreateNextContextOptions
-): Promise<Context> {
+export async function createContext(opts: CreateNextContextOptions) {
   // for API-response caching see https://trpc.io/docs/caching
 
-  return await createContextInner({})
+  return {
+    req: {
+      headers: opts.req.headers,
+    },
+  }
 }
+
+export type Context = inferAsyncReturnType<typeof createContext>
