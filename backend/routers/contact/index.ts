0a79d7ec060a8d02164853bafe9eafe43fde9740
hisho <hisho.web@gmail.com>
2023-02-06 16:13:15 +0900

    ✨ send gridでメールを送信する機能を作成

--- backend/routers/contact/index.ts
+++ backend/routers/contact/index.ts
@@ -1,13 +1,25 @@
 import { publicProcedure, router } from '@backend/trpc'
 import { createContactInput } from '@shared/contact/createContact.input'
 import { postContactChatMessage } from '@backend/lib/slack/postChatMessage'
+import { sendMail } from '@backend/lib/sendGrid/sendMail'
+import { FROM_EMAIL } from '@backend/constant'
 
 export const contactRouter = router({
   create: publicProcedure
     .input(createContactInput)
     .mutation(async ({ input }) => {
       try {
-        await postContactChatMessage(input)
+        await Promise.all([
+          postContactChatMessage(input),
+          sendMail({
+            toEmail: input.email,
+            fromEmail: FROM_EMAIL,
+            subject: 'お問い合わせありがとうございます',
+            message:
+              'お問い合わせありがとうございます\n\n後ほどご連絡いたします',
+          }),
+        ])
+
         return 'success'
       } catch (e) {
         return 'not success'
