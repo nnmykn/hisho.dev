170117164ea168d3fdf9fae8beb6d045bc17a6c9
hisho <hisho.web@gmail.com>
2023-06-04 00:01:59 +0900

    ✨ update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,17 @@
-/** @type {import('next').NextConfig} */
+/**
+ * @returns {import('next').NextConfig}
+ * */
 const nextConfig = () => {
   const __PROD__ = process.env.NODE_ENV === 'production'
 
   return {
+    poweredByHeader: false,
+    trailingSlash: true,
     reactStrictMode: true,
     experimental: {
       appDir: true,
+      typedRoutes: true,
+      serverActions: true,
     },
   }
 }
