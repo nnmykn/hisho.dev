19e7c8940bd98eac153bb7f82e24c8af97cf07be
hisho <hisho.web@gmail.com>
2023-06-03 19:44:19 +0900

    ✨ update env

--- src/constant/env.ts
+++ src/constant/env.ts
@@ -1,14 +1,15 @@
 import { createEnv } from '@t3-oss/env-nextjs'
 import { z } from 'zod'
-
 export const env = createEnv({
   server: {
     VERCEL_URL: z.string().min(1).catch(''),
-    PORT: z.coerce.number().int(),
+    PORT: z.coerce.number().int().catch(3000),
+    NODE_ENV: z.enum(['development', 'production', 'test'] as const),
   },
   client: {},
   runtimeEnv: {
     VERCEL_URL: process.env['VERCEL_URL'],
     PORT: process.env['PORT'],
+    NODE_ENV: process.env['NODE_ENV'],
   },
 })
