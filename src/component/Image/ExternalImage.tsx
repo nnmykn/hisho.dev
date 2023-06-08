a460a27517806e36d56b144b49434010ccea266c
hisho <hisho.web@gmail.com>
2023-06-09 00:11:28 +0900

    ✨ create ExternalImage component

--- src/component/Image/ExternalImage.tsx
+++ src/component/Image/ExternalImage.tsx
@@ -3,5 +3,6 @@ import { ComponentProps } from 'react'
 type Props = Pick<ComponentProps<'img'>, 'src' | 'alt' | 'className'>
 
 export const ExternalImage = ({ src, className, alt }: Props) => {
+  //eslint-disable-next-line @next/next/no-img-element
   return <img src={src} alt={alt} className={className} />
 }
