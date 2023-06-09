5c1251c491723597255cdc8066fe48bcbdcc3f7a
hisho <hisho.web@gmail.com>
2023-06-10 00:12:27 +0900

    ✨ create InternalImage

---
+++ src/component/Link/InternalLink.tsx
@@ -0,0 +1,19 @@
+import Link from 'next/link'
+import { Route } from 'next'
+import { ComponentProps } from 'react'
+
+type Props<T extends string> = Pick<
+  ComponentProps<typeof Link>,
+  'className' | 'children'
+> & {
+  href: Route<T>
+}
+/**
+ * @see https://nextjs.org/docs/app/building-your-application/configuring/typescript#statically-typed-links
+ */
+export const InternalLink = <T extends string>({
+  children,
+  ...props
+}: Props<T>) => {
+  return <Link {...props}>{children}</Link>
+}
