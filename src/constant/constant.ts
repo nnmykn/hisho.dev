b48a4b01f8f13800ad177d28cfcb6dcd695c4c0f
hisho <hisho.web@gmail.com>
2023-06-14 00:15:00 +0900

    🐛 fixed

--- src/constant/constant.ts
+++ src/constant/constant.ts
@@ -1,6 +1,6 @@
 import { env } from '@/src/constant/env'
 
 export const NEXT_PUBLIC_FRONTEND_URL =
-  env.NODE_ENV === 'production'
-    ? (`https://${env.VERCEL_URL}` as const)
-    : (`http://localhost:${env.PORT}` as const)
+  env.NEXT_PUBLIC_NODE_ENV === 'production'
+    ? (`https://${env.NEXT_PUBLIC_VERCEL_URL}` as const)
+    : (`http://localhost:${env.NEXT_PUBLIC_PORT}` as const)
