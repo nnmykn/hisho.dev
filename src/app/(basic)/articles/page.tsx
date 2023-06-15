64e0b4cd309a9c64c7136798a1776567bbde7332
hisho <hisho.web@gmail.com>
2023-06-16 05:06:01 +0900

    🔥 deployするために記事取得をコメントアウト

--- src/app/(basic)/articles/page.tsx
+++ src/app/(basic)/articles/page.tsx
@@ -1,27 +1,23 @@
-import { ExternalLink } from '@/src/component/Link/ExternalLink'
-import { fetchArticles } from '@/src/app/(basic)/articles/_feature/article/fetchArticles'
-import { ArticleCard } from '@/src/app/(basic)/articles/_feature/article/ArticleCard'
-
 const Page = async () => {
-  const articles = await fetchArticles()
+  // const articles = await fetchArticles()
   return (
     <div>
-      <ul
-        className={
-          'grid gap-3 grid-cols-[repeat(auto-fit,_minmax(360px,_1fr))]'
-        }
-      >
-        {articles.map((article, index) => (
-          <li key={`li_${article.id}_${index}`}>
-            <ExternalLink
-              className={'hover:opacity-80 transition-opacity'}
-              href={article.url}
-            >
-              <ArticleCard article={article} />
-            </ExternalLink>
-          </li>
-        ))}
-      </ul>
+      {/*<ul*/}
+      {/*  className={*/}
+      {/*    'grid gap-3 grid-cols-[repeat(auto-fit,_minmax(360px,_1fr))]'*/}
+      {/*  }*/}
+      {/*>*/}
+      {/*  {articles.map((article, index) => (*/}
+      {/*    <li key={`li_${article.id}_${index}`}>*/}
+      {/*      <ExternalLink*/}
+      {/*        className={'hover:opacity-80 transition-opacity'}*/}
+      {/*        href={article.url}*/}
+      {/*      >*/}
+      {/*        <ArticleCard article={article} />*/}
+      {/*      </ExternalLink>*/}
+      {/*    </li>*/}
+      {/*  ))}*/}
+      {/*</ul>*/}
     </div>
   )
 }
