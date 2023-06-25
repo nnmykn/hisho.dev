f220b46501a36155dbf0f3431b6526618fc9c9c9
hisho <hisho.web@gmail.com>
2023-06-25 10:06:55 +0900

    ✨ update external link

--- src/component/Link/ExternalLink.tsx
+++ src/component/Link/ExternalLink.tsx
@@ -5,7 +5,7 @@ type Props = Pick<ComponentProps<'a'>, 'children' | 'className' | 'href'>
 export const ExternalLink = forwardRef<HTMLAnchorElement, Props>(
   ({ children, ...props }, ref) => {
     return (
-      <a {...props} ref={ref}>
+      <a {...props} ref={ref} rel={'noopener noreferrer'} target={'_blank'}>
         {children}
       </a>
     )
