de5a2d0ad2f8f89f2a50bd477bacf54319366ccb
hisho <hisho.web@gmail.com>
2023-07-02 08:50:49 +0900

    ✨ dialog

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,14 @@ module.exports = {
   plugins: [require('@tailwindcss/container-queries')],
   theme: {
     extend: {
+      animation: {
+        contentShow: 'contentShow 150ms cubic-bezier(0.16, 1, 0.3, 1)',
+        overlayShow: 'overlayShow 150ms cubic-bezier(0.16, 1, 0.3, 1)',
+      },
+      animation: {
+        contentShow: 'contentShow 150ms cubic-bezier(0.16, 1, 0.3, 1)',
+        overlayShow: 'overlayShow 150ms cubic-bezier(0.16, 1, 0.3, 1)',
+      },
       /**
        * Colors
        * @see https://colorhunt.co/palette/232931393e464ecca3eeeeee
@@ -15,6 +23,16 @@ module.exports = {
         secondary: '#393E46',
         transparent: 'transparent',
         write: '#EEEEEE',
+      },
+      keyframes: {
+        contentShow: {
+          from: { opacity: 0, transform: 'translate(-50%, -48%) scale(0.96)' },
+          to: { opacity: 1, transform: 'translate(-50%, -50%) scale(1)' },
+        },
+        overlayShow: {
+          from: { opacity: 0 },
+          to: { opacity: 1 },
+        },
       },
     },
     fontFamily: {
