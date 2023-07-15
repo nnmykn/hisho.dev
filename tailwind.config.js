5878c51354843c542dabc0e1ccde07959b7eee8e
hisho <hisho.web@gmail.com>
2023-07-15 20:17:52 +0900

    🔧 update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const _ = require('lodash')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.ts', './src/**/*.tsx'],
@@ -30,9 +33,24 @@ module.exports = {
           to: { opacity: 1 },
         },
       },
+      spacing: Object.fromEntries(
+        _.range(0, 211).map((n) => [n / 2, `${n / 8}rem`])
+      ),
     },
     fontFamily: {
       body: ['system-ui'],
+    },
+    fontSize: Object.fromEntries(
+      _.range(10, 81, 1).map((n) => [n, `${n / 16}rem`])
+    ),
+    lineHeight: Object.fromEntries(
+      _.range(100, 201, 5).map((n) => [n / 100, n / 100])
+    ),
+    maxWidth: {
+      none: 'none',
+      ...Object.fromEntries(
+        _.range(0, 1201, 2).map((n) => [n / 4, `${n / 16}rem`])
+      ),
     },
   },
 }
