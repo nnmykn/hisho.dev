cfa35567f1dc4a9baf46e7bb3f79d59820b7f4d0
hisho <hisho.web@gmail.com>
2023-07-16 18:30:53 +0900

    💄 inputとtextareaのuiを調整

--- src/component/from/textarea/textarea.tsx
+++ src/component/from/textarea/textarea.tsx
@@ -6,15 +6,15 @@ import { ComponentProps, forwardRef } from 'react'
 export const Textarea = forwardRef<
   HTMLTextAreaElement,
   Pick<ComponentProps<'textarea'>, 'name' | 'onBlur' | 'onChange' | 'value'>
->(({ value, ...props }, ref) => {
+>(({ value = '', ...props }, ref) => {
   return (
     <textarea
       className={cn(
-        'block w-full text-primary',
-        'border-2 aria-[invalid="true"]:border-red'
+        'block w-full bg-transparent px-2 py-1 text-white backdrop-blur-md',
+        'border-2 border-gray-600 aria-[invalid="true"]:border-red'
       )}
       ref={ref}
-      value={value ?? ''}
+      value={value}
       {...props}
     />
   )
