4d4c04607ddb6ab285b3fc2a36238edea0cbbaad
hisho <hisho.web@gmail.com>
2024-01-01 05:33:41 +0900

    ♻️ apiを別の場所から取得するように変更

--- src/app/(basic)/events/page.tsx
+++ src/app/(basic)/events/page.tsx
@@ -1,22 +1,32 @@
-import { eventsSchema } from '@/app/(basic)/events/api/constant'
-import { FRONTEND_URL } from '@/constant/constant'
+import { env } from '@/constant/env'
+import { z } from 'zod'
 
 export const dynamic = 'force-dynamic'
 
+const eventSchema = z.object({
+  date: z.string().datetime(),
+  detail: z.string().nullable(),
+  github: z.string().nullable(),
+  slideUrl: z.string().nullable(),
+  title: z.string(),
+})
+
 export default async function () {
-  const events = await eventsSchema.parseAsync(
-    await fetch(`${FRONTEND_URL}/events/api`).then((response) =>
-      response.json()
-    )
+  const events = await z.array(eventSchema).parseAsync(
+    await fetch(`${env.BACKEND_URL}/events/`, {
+      headers: {
+        'x-authorization': env.X_AUTHORIZATION,
+      },
+    }).then((response) => response.json())
   )
 
   return (
     <div>
       {events.map((event, index) => (
-        <div key={`event_${event.name}_${index}`}>
-          <h1>{event.name}</h1>
+        <div key={`event_${event.detail}_${index}`}>
+          <h1>{event.title}</h1>
           <div>{event.date}</div>
-          <div>{event.url}</div>
+          <div>{event.detail}</div>
           <div>{event.slideUrl}</div>
         </div>
       ))}
